793921fe07a269ddb1357b34ba204c995c9d54ac
EvilKanoa <kanoa@kanoa.ca>
2022-12-30 11:06:26 -0500

    feat: complete day 17 part 2

--- day17.2.ts
+++ day17.2.ts
@@ -1,10 +1,15 @@
-import { Vector, addVectors } from "./vector";
+import {
+  SerializedVector,
+  Vector,
+  addVectors,
+  parseVector,
+  serializeVector,
+} from "./vector";
 
 const fs = require("fs");
 const { benchmark, max } = require("./utils");
 
 const NUM_ROCKS = 1_000_000_000_000;
-// const NUM_ROCKS = 2022;
 
 enum Jet {
   LEFT = "<",
@@ -56,7 +61,7 @@ const ROCK_OFFSETS: Record<RockShape, readonly Vector[]> = {
 };
 
 interface State {
-  floor: [number, number, number, number, number, number, number];
+  rocks: Set<SerializedVector>;
   jets: Jet[];
   jetIdx: number;
   shapes: RockShape[];
@@ -71,9 +76,13 @@ const parseJets = (input: string): Jet[] =>
     )
     .filter((c): c is Jet => c !== null);
 
-const isColliding = (positions: Vector[], floor: State["floor"]): boolean =>
+const isColliding = (positions: Vector[], rocks: State["rocks"]): boolean =>
   positions.some(
-    (pos) => pos[0] <= 0 || pos[0] >= 8 || floor[pos[0] - 1] >= pos[1]
+    (pos) =>
+      pos[0] <= 0 ||
+      pos[0] >= 8 ||
+      pos[1] <= 0 ||
+      rocks.has(serializeVector(pos))
   );
 
 const getPositions = (fromPos: Vector, shape: RockShape) =>
@@ -89,13 +98,13 @@ const dropRock = (state: State): void => {
     const jet = state.jets[state.jetIdx++];
     state.jetIdx = state.jetIdx % state.jets.length;
     const pushPos = addVectors(pos, jet === Jet.LEFT ? [-1, 0] : [1, 0]);
-    if (!isColliding(getPositions(pushPos, shape), state.floor)) {
+    if (!isColliding(getPositions(pushPos, shape), state.rocks)) {
       pos = pushPos;
     }
 
     // try to drop it
     const dropPos = addVectors(pos, [0, -1]);
-    if (isColliding(getPositions(dropPos, shape), state.floor)) {
+    if (isColliding(getPositions(dropPos, shape), state.rocks)) {
       // hit bottom or another rock, done dropping!
       break;
     }
@@ -108,18 +117,23 @@ const dropRock = (state: State): void => {
     0
   );
   state.highest = state.highest > top ? state.highest : top;
-  positions.forEach(([x, y]) => {
-    if (state.floor[x - 1] < y) {
-      state.floor[x - 1] = y;
-    }
+  positions.forEach((pos) => {
+    state.rocks.add(serializeVector(pos));
   });
 };
 
 const findCycle = (state: State): [number, number, number] => {
   const cache = new Map<string, [number, number]>();
 
-  const keyState = (s: State) =>
-    `${s.floor.map((y) => s.highest - y).join(",")};${s.jetIdx};${s.shapeIdx}`;
+  const keyState = (s: State) => {
+    let floor = "";
+    for (let y = state.highest; y > state.highest - 30; y--) {
+      for (let x = 1; x < 8; x++) {
+        floor += s.rocks.has(serializeVector([x, y])) ? "1" : "0";
+      }
+    }
+    return `${floor};${s.jetIdx};${s.shapeIdx}`;
+  };
 
   for (let rock = 1; true; rock++) {
     dropRock(state);
@@ -141,7 +155,7 @@ let result;
 
 benchmark(() => {
   const state: State = {
-    floor: [0, 0, 0, 0, 0, 0, 0],
+    rocks: new Set(),
     jets: parseJets(input),
     jetIdx: 0,
     shapes: [
@@ -156,22 +170,37 @@ benchmark(() => {
   };
 
   const [cycleStart, cycleLength, cycleHeight] = findCycle(state);
-
+  let offsetHeight = 0;
   console.log(`Using cycle:`, { cycleStart, cycleLength, cycleHeight });
 
   for (let rock = cycleStart + cycleLength + 1; rock <= NUM_ROCKS; rock++) {
     if (rock + cycleLength > NUM_ROCKS) {
+      if (offsetHeight > 0) {
+        const newRocks = new Set<SerializedVector>();
+        state.rocks.forEach((posStr) => {
+          const pos = parseVector(posStr);
+          newRocks.add(serializeVector([pos[0], pos[1] + offsetHeight]));
+        });
+        state.rocks = newRocks;
+        offsetHeight = 0;
+      }
+
       dropRock(state);
-    } else {
+    } else if (rock + cycleLength * 100 > NUM_ROCKS) {
       // apply a cycle
       rock += cycleLength - 1;
+      offsetHeight += cycleHeight;
       state.highest += cycleHeight;
-      state.floor.forEach((y, idx) => (state.floor[idx] = y + cycleHeight));
+    } else {
+      // apply 100 cycles
+      rock += cycleLength * 100 - 1;
+      offsetHeight += cycleHeight * 100;
+      state.highest += cycleHeight * 100;
     }
   }
 
   result = state.highest;
-}, true);
+});
 
 console.log(result);
 
